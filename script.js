9a30055217c229941511f2dca8e6bde359d2475e
SarahRazzaq <sarah.razzaq@stud.sundsgarden.se>
2022-05-22 16:06:09 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 16;
+
+const isOfAge = age >= 18;
+
+if (isOfAge){
+    console.log("Yay!!! Your old enough to get a license!!");
+}
+else {
+    console.log("Sorry, you have to wait a few years");
+}
+if (age >= 18) {
+    console.log("Yay! Your're ready to take a license")
+}else {
+    const yearsLeft = 18 - age;
+    console.log(`Sorry, your're to young. Wait another ${yearsLeft} years!`);
+}
+if (age>=18) {
+    console.log("Yay! Your're ready to take a license")
+} else {
+    
+}
